13827302bdcd30355781f3261a7192e9d72aa0d1
ProdigalCoder456 <Joshua.a.moore02@gmail.com>
2025-05-27 13:15:09 -0500

    Attempt to display the data contents.

--- VideoGameLibrary/src/App.jsx
+++ VideoGameLibrary/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -21,37 +19,25 @@ function App() {
         }
     })
     console.log(gameData)
-    
   }
-   useEffect(() => {
-    getData()
-   },[]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Video Game Library</h1>
+      <div className="games-list">
+        {games.map((game) => (
+          <div key={game.id} className="game-card">
+            <img src={game.background_image} alt={game.name} className="game-image" />
+            <h2>{game.name}</h2>
+            <p>Metacritic: {game.metacritic}</p>
+            <p>Released: {game.released}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+  
+      
 
 export default App
 
