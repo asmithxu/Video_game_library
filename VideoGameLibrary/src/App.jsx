3fa0d2db0e0e8e32ef855a36d8d7d1f7d68ad41b
Jiyah <jiyahcoleman00@gmail.com>
2025-05-27 11:37:13 -0500

    update
    getData added

--- VideoGameLibrary/src/App.jsx
+++ VideoGameLibrary/src/App.jsx
@@ -6,6 +6,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+    async function getData() {
+    const response = await fetch("https://api.rawg.io/api/games?key=2a81c7e237774deeb08ce07a5fc6cb15&ordering='metacritic'&page_size=30")
+    let responseData = await response.json()
+    responseData = responseData.results
+    console.log(responseData)
+    let gameData = await responseData.map((games) => {
+        return {
+          name: games.name,
+          background_image: games.background_image,
+          metacritic: games.metacritic,
+          released: games.released,
+          id: games.id
+        }
+    })
+    console.log(gameData)
+
   return (
     <>
       <div>
@@ -33,3 +49,4 @@ function App() {
 }
 
 export default App
+
