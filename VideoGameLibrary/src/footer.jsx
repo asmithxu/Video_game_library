d3e738ce79d56d8812f21227c46ccb2c99915ade
ProdigalCoder456 <Joshua.a.moore02@gmail.com>
2025-05-28 10:44:26 -0500

    Footer

---
+++ VideoGameLibrary/src/footer.jsx
@@ -0,0 +1,2 @@
+
+<p> Copyright Joshua Moore, Jiyah S, and Ayana Coleman</p>
--- VideoGameLibrary/src/header.jsx
+++ VideoGameLibrary/src/header.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './header.css';
 
-export default function Header() {
   return (
     <header className="header">
       <h1>Video Game Library</h1>
       <p>Welcome to the Video Game Library! Here you can find a collection of your favorite video games.</p>
     </header>
   );
-}
+export default Header;
 
 
