d7a2e49ba875d3d4e3c622491596d180f65f4ab1
asmithxu <93105197+asmithxu@users.noreply.github.com>
2025-05-28 11:53:44 -0500

    Add route to get one user and route to post a user

--- server/App.js
+++ server/App.js
@@ -14,16 +14,59 @@ const client = new MongoClient(mongodb_uri)
 app.use(cors())
 app.use(express.json())
 
+//Get all of the user data
 app.get("/userdata", async (request,response)=> {
     try {
         const database = await client.db(db);
         const coll = await database.collection(collection);
         const data = await coll.find().toArray()
 
+        // console.log(data)
+
         response.status(200).send(data)
     } catch (error) {
         console.error("Something went wrong: ", error)
     }
+})
+
+//Get one user. This can be used to grab their info once they login.
+app.get("/userdata/:username", async (request,response)=>{
+    try {
+        const database = await client.db(db);
+        const coll = await database.collection(collection);
+        const data = await coll.find().toArray()
+
+        let userName = request.params.username
+
+        const usernameData = data.filter((user) =>{
+            return user.username === userName
+        })
+
+        // Conditionally send a response based on whether that data is found or not
+        if (usernameData.length > 0){
+        let json_response = JSON.stringify(usernameData)
+        response.status(200).send(json_response)
+        }else{
+        response.status(404).send("Resource not found")
+        };
+
+    } catch (error) {
+        console.error("Something went wrong: ", error)
+    }
+    
+})
+
+// Create a new user
+app.post("/userdata", async (request, response)=>{
+    let newUser = {...request.body};
+    const database = await client.db(db);
+    const coll = await database.collection(collection);
+    await coll.insertOne(newUser)
+
+  
+    let data = [newUser]
+    console.log('Received data', newUser)
+    response.status(201).send(JSON.stringify([...data]))
 })
 
 // Start the server
